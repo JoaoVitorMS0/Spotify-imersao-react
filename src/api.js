c01721824026712595a8663ff480faaa607cbd80
𝓙𝓸𝓪𝓸 𝓥𝓲𝓽𝓸𝓻  <jvmendonca10@hotmail.com>
2025-02-01 04:25:25 -0300

    Atualização na Api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 // api.js
 export function requestApi(searchTerm) {
-    const url = process.env.REACT_APP_API_URL;
+    const url = process.env.REACT_APP_API_URL; //Link mocado da API
     
     return fetch(url)
         .then((response) => response.json())
